e5ecd593bfd36cc90f123058d8a658b5ade95860
Wai Kit <cwkitkat@gmail.com>
2018-09-04 23:19:37 +0800

    - new player controller using buttons

---
+++ MMMG Prototype/Assets/MovementController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+	[SerializeField] SpriteRenderer m_spriteRend = null;
+	Rigidbody m_rb;
+	private float hAxis;
+	[SerializeField] private float speed = 5;
+	private float m_speed;
+	private Vector3 hMovement;
+	private bool isLeft, isRight;
+
+	private void OnEnable(){
+		m_rb = GetComponent<Rigidbody>();
+	}
+
+	private void Update(){
+		DecideDirection ();
+		CalculateMovement ();
+	}
+
+	private void FixedUpdate(){
+		MoveRb ();
+	}
+
+	public void MoveLeft(){
+		isLeft = true;
+		FlipLeft ();
+	}
+
+	public void ResetMovement(){
+		isLeft = false;
+		isRight = false;
+	}
+
+	public void MoveRight(){
+		isRight = true;
+		FlipRight ();
+	}
+
+	private void DecideDirection(){
+		if (isLeft)
+		{
+			hAxis = -1;
+		}
+		else if (isRight)
+		{
+			hAxis = 1;
+		}
+		else
+		{
+			hAxis = 0;
+		}
+	}
+
+	private void CalculateMovement(){
+		m_speed = speed * Time.deltaTime;
+		hMovement = new Vector3 (hAxis, 0, 0) * m_speed;
+	}
+
+	private void MoveRb(){
+		m_rb.MovePosition (transform.position + hMovement);
+	}
+
+	private void FlipLeft(){
+		m_spriteRend.flipX = true;
+	}
+	private void FlipRight(){
+		m_spriteRend.flipX = false;
+	}
+}
--- MMMG Prototype/Assets/Scripts/PlayerController.cs
+++ MMMG Prototype/Assets/Scripts/PlayerController.cs
@@ -53,59 +53,16 @@ public class PlayerController : MonoBehaviour {
 		directionX = CrossPlatformInputManager.GetAxis (s_horizontal);
 	}
 
-	//For Gravity Related
-		//GetAxisRaw dont have float in between 0 to 1, GetAxis has float in between from 0 before reaching 1
-		//use GetAxis on ground, use GetAxisRaw in air
-		//unfreeze if no ground / not touching anything
-		//m_rb.constraints = RigidbodyConstraints.None;
-
-	//horizontal movement is move along z axis
-	//lateral movement is move along x axis
-	//vertical movement is move along y axis
-
-	//along x-axis
-	//i lazy test :P
-
-	//along y-axis
 	Quaternion y_left = Quaternion.Euler (0, 180, 0);
 	Quaternion y_right = Quaternion.Euler (0, 0, 0);
-//	Quaternion y_back = Quaternion.Euler (0, 90, 0);
-//	Quaternion y_front = Quaternion.Euler (0, 270, 0);
-//
-//	//anti-clockwise
 	Quaternion y_left1 = Quaternion.Euler(0,181,0);
 	Quaternion y_right1 = Quaternion.Euler(0,1,0);
-//	//clockwise
-//	Quaternion y_left2 = Quaternion.Euler(0,180,0);
-//	Quaternion y_right2 = Quaternion.Euler(0,0,0);
-//
-//	//along  z-axis
-//	Quaternion z_left = Quaternion.Euler (0, 0, 180);
-//	Quaternion z_right = Quaternion.Euler (0, 0, 0);
-//	Quaternion z_up = Quaternion.Euler (0, 0, 90);
-//	Quaternion z_down = Quaternion.Euler (0, 0, 270);
-
-	//set specific direction, then use RotateTowards that dir
-		//Quaternion q = Quaternion.LookRotation(target.position - transform.position);
-
-	//Slerp: rotate with abit motion afetrwards
-		//player.rotation = Quaternion.Slerp (player.rotation, y_right, Time.deltaTime);
-	//SlerpUnclamped: slerp but faster?
-		//player.rotation = Quaternion.SlerpUnclamped (player.rotation, y_right, Time.deltaTime);
-	//RotateTowards: so slow...
-		//player.rotation = Quaternion.RotateTowards (player.rotation, y_right, Time.deltaTime);
-
-	//FromToRotation: unadjustable speed which look at the target position instantly
-		//player.rotation = Quaternion.FromToRotation((player.position - Vector3.right), player.position);
-
 	private void FixedUpdate()
 	{
 		if (isPlaying) {
 			m_rb.MovePosition (transform.position + hMovement);
 			ResetPlayerRotation (transform);
-			//CameraMovement (cam_min_x, cam_max_x);
 		}
-
 		m_rb.velocity = new Vector2 (directionX * moveSpeed, m_rb.velocity.y);
 	}
 
@@ -136,41 +93,23 @@ public class PlayerController : MonoBehaviour {
 	{
 		if (Input.GetButton (s_horizontal))
 		{
-			//freeze x,y,z pos b4 use to avoid collision
-//			if (Input.GetAxis (s_horizontal) > 0 && Input.GetAxis (s_horizontal) < 1) {
-//				player.rotation = Quaternion.Slerp (player.rotation, y_right1, rotationSpeed * Time.deltaTime);
-//			}
-//			if (Input.GetAxis (s_horizontal) > -1 && Input.GetAxis (s_horizontal) < 0) {
-//				player.rotation = Quaternion.Slerp (player.rotation, y_left1, rotationSpeed * Time.deltaTime);
-//			}
-//			if (Input.GetAxis (s_horizontal) == 1) {
-//				player.rotation = Quaternion.Slerp (player.rotation, y_right, rotationSpeed * Time.deltaTime);
-//			}
-//			if (Input.GetAxis (s_horizontal) == -1) {
-//				player.rotation = Quaternion.Slerp (player.rotation, y_left, rotationSpeed * Time.deltaTime);
-//			}
-
 			clamp = false;
 			m_clampCooldown = clampCooldown;
 			if (Input.GetAxis (s_horizontal) > 0 && Input.GetAxis (s_horizontal) < 1)
 			{
 				player.rotation = Quaternion.RotateTowards (player.rotation, y_right1, rotationSpeed);
-				//anim.SetBool ("isRunningRight", true);
 			}
 			if (Input.GetAxis (s_horizontal) > -1 && Input.GetAxis (s_horizontal) < 0)
 			{
 				player.rotation = Quaternion.RotateTowards (player.rotation, y_left1, rotationSpeed);
-				//anim.SetBool ("isRunningLeft", true);
 			}
 			if (Input.GetAxis (s_horizontal) == 1)
 			{
 				player.rotation = Quaternion.RotateTowards (player.rotation, y_right, rotationSpeed);
-				//anim.SetBool ("isRunningRight", true);
 			}
 			if (Input.GetAxis (s_horizontal) == -1)
 			{
 				player.rotation = Quaternion.RotateTowards (player.rotation, y_left, rotationSpeed);
-				//anim.SetBool ("isRunningLeft", true);
 			}
 		}
 		else {
@@ -179,8 +118,6 @@ public class PlayerController : MonoBehaviour {
 			} else {
 				m_clampCooldown -= Time.deltaTime;
 			}
-//			anim.SetBool ("isRunningRight", false);
-//			anim.SetBool ("isRunningLeft", false);
 		}
 
 		if (CrossPlatformInputManager.GetButton (s_horizontal))
