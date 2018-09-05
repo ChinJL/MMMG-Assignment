924da5895c40a8da964431565b428fc892fbf0b3
FireH3art <jee.kiew@hotmail.com>
2018-09-05 15:20:11 +0800

    Level2

---
+++ MMMG Prototype/Assets/Scripts/Level2Unlock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level2Unlock : MonoBehaviour {
+
+	public GameObject Nextleveldoor;
+	// Use this for initialization
+//	void OnEnable () {
+//		if (Nextleveldoor == null)
+//		{
+//			
+//		}
+//	}
+	
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "True")
+		{
+			Nextleveldoor = GameObject.Find ("SecretDoor");
+			print ("hi");
+			Nextleveldoor.SetActive(true);
+		}
+	}
+
+}
---
+++ MMMG Prototype/Assets/Scripts/Platform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Fake")
+		{
+			Destroy (gameObject);
+		}
+	}
+
+
+}
