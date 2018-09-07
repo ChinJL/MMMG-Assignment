051f19026af21f3ca7418e1fd4632fa1eeaa5ad2
Wai Kit <cwkitkat@gmail.com>
2018-09-07 11:20:28 +0800

    *please backup before pulling, i hope none of the files from other levels being affected* *decorate your level as you please*
    Level 3
    - Reworked entire Laser system (Laser Cannon, Laser Reflect, Laser Receiver, Laser Portal)
    - Laser system now using inheritence (OOP) //lolz
    - Laser Reflect, Laser Receiver and Laser Portal have power level, powered by laser raycast
    - Modified appearance of laser gun, reflector, laser sensor and portal
    - Portal now can be reflected vertically
    - Added 2 Particle Effect: Bullets for Portal Gun when shooting and Rooms' Floor when rooms are arranged correctly
    - Modified Switch Room: Hide player controls when switching
    - Added Inventory UI
    - Portal Gun now work with UI
    - Added a lot of sound
    - Rework condition for door unlocking system
    
    General
    - Reworked character movement
    - Created Audio Manager
    - Added Sound Effects for Audio Manager
    - Audio Manager's play volume can be adjust using buttons and sprites, required further implement when main menu is done
    - Removed unwanted files, unused sprites and cross platform input folder
    - Modified Switch Level component in Door Trigger script because the integer cannot be increment forever (no scene after level 3)
    
    Level 2
    - Fixed character movement
    - Fixed door system

---
+++ MMMG Prototype/Assets/Scripts/Level 3/LaserSystem/Laser.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+	protected Transform laserPoint, laserSource;
+	protected Vector3 direction;
+
+	private string s_laserCannon = "LaserCannon", s_laserReflect = "LaserReflect", s_laserSensor = "LaserSensor", s_laserPortal = "LaserPortal";
+
+	//Vector3 heading, float distance, Vector3 direction,
+	protected virtual void AdjustDirection(){
+		Vector3 heading = laserPoint.position - laserSource.position;
+		float distance = heading.magnitude;
+		direction = heading / distance;
+	}
+
+	//Transform laserPoint, Vector3 direction
+	protected virtual void ShootLaser(){
+		Ray laserRay = new Ray (laserPoint.position, direction);
+		RaycastHit hit;
+		if (Physics.Raycast (laserRay, out hit, Mathf.Infinity))
+		{
+			GameObject hitObject = hit.transform.gameObject;
+			if (hitObject != null)
+			{
+				DisplayLaser (hit.point, hitObject);
+			}
+		}
+	}
+
+	protected virtual void DisplayLaser(Vector3 hitPos, GameObject hitObj){
+		Debug.DrawLine (laserPoint.position, hitPos, Color.green);
+		//effect
+		if (gameObject.CompareTag (s_laserCannon))
+		{
+			if (hitObj.CompareTag (s_laserReflect))
+			{
+				laserReflector = hitObj;
+				LaserEffect1 ();
+			}
+			else if (hitObj.CompareTag (s_laserPortal))
+			{
+				portal = hitObj;
+				LaserEffect3 ();
+			}
+		}
+		else if (gameObject.CompareTag (s_laserReflect))
+		{
+			if (hitObj.CompareTag (s_laserSensor))
+			{
+				laserSensor = hitObj;
+				LaserEffect2 ();
+			}
+			else if (hitObj.CompareTag (s_laserPortal))
+			{
+				portal = hitObj;
+				LaserEffect3 ();
+			}
+		}
+		else if (gameObject.CompareTag (s_laserPortal))
+		{
+			if (hitObj.CompareTag (s_laserReflect))
+			{
+				laserReflector = hitObj;
+				LaserEffect1 ();
+			}
+			else if (hitObj.CompareTag (s_laserSensor))
+			{
+				laserSensor = hitObj;
+				LaserEffect2 ();
+			}
+			else if (hitObj.CompareTag (s_laserPortal))
+			{
+				portal = hitObj;
+				LaserEffect3 ();
+			}
+		}
+	}
+
+	protected GameObject laserReflector;
+	protected virtual void LaserEffect1(){
+		//reflector
+	}
+
+	protected GameObject laserSensor;
+	protected virtual void LaserEffect2(){
+		//sensor
+	}
+
+	protected GameObject portal;
+	protected virtual void LaserEffect3(){
+		//portal
+	}
+}
